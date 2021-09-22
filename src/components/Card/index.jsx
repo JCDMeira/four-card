68a39e2177f3aca64254ad73e63856b05751171f
Jean Meira <jean.meira10@hotmail.com>
2021-09-22 11:44:32 -0300

    add: camponent cards and styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './style.css';
 
 function Card({ tittle, image, color, children }) {
   return (
-    <div className={`card ${color}`}>
+    <div className={`card color-${color}`}>
       <h1>{tittle}</h1>
       <p>{children}</p>
       <img src={image} alt="icone que descreve o campo card" />
